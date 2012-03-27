43cda0bd48ce34cc4d8b10ac7cd6cd9af2f73f9c
shaunfreeman <shaun@shaunfreeman.co.uk>
2012-03-27 19:32:16 +0100

    simplified this model, all decorators and other functallity pushed to concete classes

--- Form/Abstract.php
+++ Form/Abstract.php
@@ -4,34 +4,34 @@
  *
  * Copyright (c) 2011 Shaun Freeman <shaun@shaunfreeman.co.uk>.
  *
- * This file is part of Uthando-CMS.
+ * This file is part of ZendSF.
  *
- * Uthando-CMS is free software: you can redistribute it and/or modify
+ * ZendSF is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Uthando-CMS is distributed in the hope that it will be useful,
+ * ZendSF is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Uthando-CMS.  If not, see <http://www.gnu.org/licenses/>.
+ * along with ZendSF.  If not, see <http://www.gnu.org/licenses/>.
  *
- * @category ZendSF
- * @package ZendSF
+ * @category   ZendSF
+ * @package    ZendSF
  * @subpackage Form
- * @author Shaun Freeman <shaun@shaunfreeman.co.uk>
+ * @author     Shaun Freeman <shaun@shaunfreeman.co.uk>
  */
 
 /**
  * Base class for forms.
  *
- * @category ZendSF
- * @package UZendSF
+ * @category   ZendSF
+ * @package    ZendSF
  * @subpackage Form
- * @author Shaun Freeman <shaun@shaunfreeman.co.uk>
+ * @author     Shaun Freeman <shaun@shaunfreeman.co.uk>
  */
 abstract class ZendSF_Form_Abstract extends Zend_Form
 {
@@ -40,218 +40,13 @@ abstract class ZendSF_Form_Abstract extends Zend_Form
      */
     protected $_model;
 
-    protected $_captchaDecorators = array(
-        'Errors',
-        array(
-            array('data' => 'HtmlTag'),
-            array(
-                'tag'   => 'p',
-                'class' => 'element'
-            )
-        ),
-        array(
-            'Label',
-            array('tag' => 'p')
-        ),
-        array(
-            array('row' => 'HtmlTag'),
-            array(
-                'tag'   => 'div',
-                'id'    => 'captcha'
-            )
-        )
-    );
-
-    protected $_defaultDecorators = array(
-        'Description',
-        'FormElements',
-        array(
-            'HtmlTag',
-            array(
-                'tag'   => 'div',
-                'class' => 'zend_form'
-            )
-        ),
-        'DijitForm'
-    );
-
-    /**
-     * An array that set the global decorators for all elements.
-     *
-     * @var array
-     */
-    protected $_elementDecorators = array(
-        'ViewHelper',
-        'Errors',
-        'Description',
-        array(
-            array('data' => 'HtmlTag'),
-            array(
-                'tag' => 'p',
-                'class' => 'element'
-            )
-        ),
-        array(
-            'Label',
-            array('tag' => 'p')
-        ),
-        array(
-            array('row' => 'HtmlTag'),
-            array(
-                'tag' => 'div',
-                'class' => 'form_row'
-            )
-        )
-    );
-
-    protected $_fileDecorators = array(
-        'File',
-        'Errors',
-        'Description',
-        array(
-            array('data' => 'HtmlTag'),
-            array(
-                'tag' => 'p',
-                'class' => 'element'
-            )
-        ),
-        array(
-            'Label',
-            array('tag' => 'p')
-        ),
-        array(
-            array('row' => 'HtmlTag'),
-            array('tag' => 'div')
-        )
-    );
-
-    protected $_hashDecorators = array(
-        'ViewHelper',
-        array(
-            'HtmlTag',
-            array('tag' => 'div')
-        )
-    );
-
-    protected $_hiddenDecorators = array(
-        'ViewHelper',
-        array(
-            'HtmlTag',
-            array('tag' => 'div')
-        )
-    );
-
-    protected $_submitDecorators = array(
-        'ViewHelper',
-        array(
-            'HtmlTag',
-            array('tag' => 'span')
-        )
-    );
-
-    /**
-     * Loads the default form decorators.
-     */
-    public function loadDefaultDecorators()
-    {
-        $this->setDecorators($this->_defaultDecorators);
-    }
-
-    /**
-     * Constructs a submit button.
-     *
-     * @param string $label
-     * @param string $attribs elements attributes
-     * @return ZendSF_Form_Abstract
-     */
-    public function addSubmit($label, $name = 'submit', $attribs = array())
-    {
-        $this->addElement('SubmitButton', $name, array(
-            'ignore'        => true,
-            'required'      => false,
-            'decorators'    => $this->_submitDecorators,
-            'label'         => $label,
-            'attribs'       => $attribs
-        ));
-
-       return $this;
-    }
-
-    /**
-     * Adds a hidden element to the form.
-     *
-     * @param string $id id or name of element.
-     * @param string $value value to be inserted into the hidden element.
-     * @return ZendSF_Form_Abstract
-     */
-    public function addHiddenElement($id, $value)
-    {
-        $this->addElement('hidden', $id, array(
-            'value'         => $value,
-            'decorators'    => $this->_hiddenDecorators
-        ));
-
-        return $this;
-    }
-
-    /**
-     * Injects a hash input element into the form to protect against CSRF attacks.
-     *
-     * @param string $id id or name of element.
-     * @return ZendSF_Form_Abstract
-     */
-    public function addHash($id)
-    {
-        $this->addElement('hash', $id, array(
-            'ignore'        => true,
-            'salt'          => 'unique',
-            'decorators'    => $this->_hashDecorators
-        ));
-
-        return $this;
-    }
-
-    /**
-     * Adds the Captcha element to the form.
-     *
-     * @param array $data array of options to apply for the captcha
-     * @param string $class elements class name
-     */
-    public function addCaptcha($data, $class = '')
-    {
-        $this->addElement('captcha', 'captcha', array(
-            'captcha'    => $data,
-            'required'   => true,
-            'label'      => _('Please enter the letters displayed below:'),
-            'attribs'    => array ('class' => $class),
-            'decorators' => $this->_captchaDecorators
-        ));
-    }
-
-    /**
-     * Excludes the email from validating against the database.
-     *
-     * @param string $email
-     * @return ZendSF_Form_Abstract
-     * @access public
-     */
-    public function excludeEmailFromValidation($element, $exclude)
-    {
-        $val = $this->getElement($element)
-                ->getValidator('Db_NoRecordExists')
-                ->setExclude($exclude);
-
-        return $this;
-    }
-
     /**
      * Model setter
      *
-     * @param object $model
+     * @param ZendSF_Model_Abstract
      * @return none
-     * @access public
      */
-    public function setModel($model)
+    public function setModel(ZendSF_Model_Abstract $model)
     {
         $this->_model = $model;
     }
@@ -259,8 +54,7 @@ abstract class ZendSF_Form_Abstract extends Zend_Form
     /**
      * Model Getter
      *
-     * @return object model
-     * @access public
+     * @return ZendSF_Model_Abstract
      */
     public function getModel()
     {
